98b9e039d770f84839eb85a778283bf25b9389d8
Alexander Fritsch <alexander.fritsch@gt-arc.com>
2024-09-14 13:47:43 +0200

    hardcode params

--- src/zed-package/src/zed_camera_node.cpp
+++ src/zed-package/src/zed_camera_node.cpp
@@ -12,90 +12,8 @@ public:
     ZedCameraNode()
         : Node("zed_camera_node")
     {
-        // Read the calibration parameters
-        std::string strSettingsFile = "/root/colcon_ws/src/zed-package/ZED2.yaml";
-
-        cv::FileStorage fsSettings(strSettingsFile, cv::FileStorage::READ);
-        if (!fsSettings.isOpened())
-        {
-            RCLCPP_ERROR(this->get_logger(), "Wrong path to settings");
-        }
-
-        // Output the entire file contents
-        std::stringstream ss;
-        ss << fsSettings; // Print the content of the file to the stringstream
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "File contents:\n%s", ss.str().c_str());
-
-        // Create Rectification maps
-        cv::Mat K_l, K_r, P_l, P_r, R_l, R_r, D_l, D_r;
-        fsSettings["LEFT.K"] >> K_l;
-        fsSettings["RIGHT.K"] >> K_r;
-
-        fsSettings["LEFT.P"] >> P_l;
-        fsSettings["RIGHT.P"] >> P_r;
-
-        fsSettings["LEFT.R"] >> R_l;
-        fsSettings["RIGHT.R"] >> R_r;
-
-        fsSettings["LEFT.D"] >> D_l;
-        fsSettings["RIGHT.D"] >> D_r;
-
-        int rows_l = fsSettings["LEFT.height"];
-        int cols_l = fsSettings["LEFT.width"];
-        int rows_r = fsSettings["RIGHT.height"];
-        int cols_r = fsSettings["RIGHT.width"];
-
-        if (K_l.empty())
-        {
-            RCLCPP_ERROR(this->get_logger(), "LEFT.K is empty");
-        }
-        if (K_r.empty())
-        {
-            RCLCPP_ERROR(this->get_logger(), "RIGHT.K is empty");
-        }
-        if (P_l.empty())
-        {
-            RCLCPP_ERROR(this->get_logger(), "LEFT.P is empty");
-        }
-        if (P_r.empty())
-        {
-            RCLCPP_ERROR(this->get_logger(), "RIGHT.P is empty");
-        }
-        if (R_l.empty())
-        {
-            RCLCPP_ERROR(this->get_logger(), "LEFT.R is empty");
-        }
-        if (R_r.empty())
-        {
-            RCLCPP_ERROR(this->get_logger(), "RIGHT.R is empty");
-        }
-        if (D_l.empty())
-        {
-            RCLCPP_ERROR(this->get_logger(), "LEFT.D is empty");
-        }
-        if (D_r.empty())
-        {
-            RCLCPP_ERROR(this->get_logger(), "RIGHT.D is empty");
-        }
-        if (rows_l == 0)
-        {
-            RCLCPP_ERROR(this->get_logger(), "LEFT.height is 0");
-        }
-        if (rows_r == 0)
-        {
-            RCLCPP_ERROR(this->get_logger(), "RIGHT.height is 0");
-        }
-        if (cols_l == 0)
-        {
-            RCLCPP_ERROR(this->get_logger(), "LEFT.width is 0");
-        }
-        if (cols_r == 0)
-        {
-            RCLCPP_ERROR(this->get_logger(), "RIGHT.width is 0");
-        }
-
-        cv::initUndistortRectifyMap(K_l, D_l, R_l, P_l.rowRange(0, 3).colRange(0, 3), cv::Size(cols_l, rows_l), CV_32F, M1l, M2l);
-        cv::initUndistortRectifyMap(K_r, D_r, R_r, P_r.rowRange(0, 3).colRange(0, 3), cv::Size(cols_r, rows_r), CV_32F, M1r, M2r);
+        // Initialize camera calibration parameters
+        initializeCalibrationParams();
 
         // Initialize publishers
         left_image_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/camera/left/image_rect", 10);
@@ -130,12 +48,47 @@ public:
     }
 
 private:
+    const int IMAGE_WIDTH = 640;
+    const int IMAGE_HEIGHT = 480;
+
+    void initializeCalibrationParams()
+    {
+        // LEFT camera intrinsic matrix (K)
+        K_l = (cv::Mat_<double>(3, 3) << 351.553, 0, 332.793, 0, 468.532, 242.539, 0, 0, 1);
+
+        // RIGHT camera intrinsic matrix (K)
+        K_r = (cv::Mat_<double>(3, 3) << 351.577, 0, 318.017, 0, 468.509, 248.872, 0, 0, 1);
+
+        // LEFT and RIGHT rectification matrices
+        R_l = cv::Mat::eye(3, 3, CV_64F); // Identity matrix
+        R_r = cv::Mat::eye(3, 3, CV_64F);
+
+        // LEFT and RIGHT distortion coefficients
+        D_l = (cv::Mat_<double>(1, 5) << -0.0406, 0.0095, -0.0006, -0.0001, -0.0047);
+        D_r = (cv::Mat_<double>(1, 5) << -0.0414, 0.0094, 0.0001, 0.0001, -0.0046);
+
+        // LEFT and RIGHT projection matrices
+        P_l = (cv::Mat_<double>(3, 4) << 351.553, 0, 332.793, 0, 0, 468.532, 242.539, 0, 0, 0, 1, 0);
+        P_r = (cv::Mat_<double>(3, 4) << 351.577, 0, 318.017, 120, 0, 468.509, 248.872, 0, 0, 0, 1, 0);
+
+        // Create rectification maps
+        cv::initUndistortRectifyMap(K_l, D_l, R_l, P_l(cv::Range(0, 3), cv::Range(0, 3)), cv::Size(IMAGE_WIDTH, IMAGE_HEIGHT), CV_32F, M1l, M2l);
+        cv::initUndistortRectifyMap(K_r, D_r, R_r, P_r(cv::Range(0, 3), cv::Range(0, 3)), cv::Size(IMAGE_WIDTH, IMAGE_HEIGHT), CV_32F, M1r, M2r);
+    }
+
     void captureAndPublish()
+    {
+        captureAndPublishIMU();
+        captureAndPublishImages();
+    }
+
+    void captureAndPublishIMU()
     {
         // Capture and publish IMU data
         sl_oc::sensors::data::Imu imu_data = sensor_capture_.getLastIMUData(5000);
         auto imu_msg = sensor_msgs::msg::Imu();
         imu_msg.header.stamp = this->now();
+        imu_msg.header.frame_id = "imu_frame"; // Make sure to set the frame ID properly
         imu_msg.linear_acceleration.x = imu_data.aX;
         imu_msg.linear_acceleration.y = imu_data.aY;
         imu_msg.linear_acceleration.z = imu_data.aZ;
@@ -144,49 +97,36 @@ private:
         imu_msg.angular_velocity.z = imu_data.gZ;
         imu_pub_->publish(imu_msg);
 
-        // Log the IMU data
-        RCLCPP_INFO(this->get_logger(), "IMU data: aX=%f, aY=%f, aZ=%f, gX=%f, gY=%f, gZ=%f",
-                    imu_data.aX, imu_data.aY, imu_data.aZ, imu_data.gX, imu_data.gY, imu_data.gZ);
+        RCLCPP_DEBUG(this->get_logger(), "IMU data: aX=%f, aY=%f, aZ=%f, gX=%f, gY=%f, gZ=%f",
+                     imu_data.aX, imu_data.aY, imu_data.aZ, imu_data.gX, imu_data.gY, imu_data.gZ);
+    }
 
-        // Capture and publish video frames
+    void captureAndPublishImages()
+    {
         cv::Mat frameYUV, frameBGR, left_raw, right_raw;
         const sl_oc::video::Frame frame = video_capture_.getLastFrame();
 
-        // Check if frame is valid before processing
         if (frame.data != nullptr && frame.width > 0 && frame.height > 0)
         {
+            // Convert and split the stereo image
             frameYUV = cv::Mat(frame.height, frame.width, CV_8UC2, frame.data);
             cv::cvtColor(frameYUV, frameBGR, cv::COLOR_YUV2BGR_YUYV);
-
             left_raw = frameBGR(cv::Rect(0, 0, frameBGR.cols / 2, frameBGR.rows));
             right_raw = frameBGR(cv::Rect(frameBGR.cols / 2, 0, frameBGR.cols / 2, frameBGR.rows));
 
-            // Resize images to 1104x621 (compressed format)
+            // Resize the images
             cv::Mat left_resized, right_resized;
-            cv::resize(left_raw, left_resized, cv::Size(640, 480));
-            cv::resize(right_raw, right_resized, cv::Size(640, 480));
+            cv::resize(left_raw, left_resized, cv::Size(IMAGE_WIDTH, IMAGE_HEIGHT));
+            cv::resize(right_raw, right_resized, cv::Size(IMAGE_WIDTH, IMAGE_HEIGHT));
 
-            // Log left_resized and right_resized frame dimensions
-            RCLCPP_INFO(this->get_logger(), "Left resized frame data: width=%d, height=%d", left_resized.cols, left_resized.rows);
-            RCLCPP_INFO(this->get_logger(), "Right resized frame data: width=%d, height=%d", right_resized.cols, right_resized.rows);
-
-            // Rectify the resized images using remap
+            // Rectify the images
             cv::Mat left_rectified, right_rectified;
             cv::remap(left_resized, left_rectified, M1l, M2l, cv::INTER_LINEAR);
             cv::remap(right_resized, right_rectified, M1r, M2r, cv::INTER_LINEAR);
 
-            // Convert rectified images to ROS Image messages
-            sensor_msgs::msg::Image::SharedPtr left_rectified_msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", left_rectified).toImageMsg();
-            sensor_msgs::msg::Image::SharedPtr right_rectified_msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", right_rectified).toImageMsg();
-
-            // Set timestamps for the rectified images
-            left_rectified_msg->header.stamp = this->now();
-            right_rectified_msg->header.stamp = this->now();
-
             // Publish the rectified images
-            RCLCPP_INFO(this->get_logger(), "Publishing rectified left and right images");
-            left_image_pub_->publish(*left_rectified_msg);
-            right_image_pub_->publish(*right_rectified_msg);
+            publishImage(left_rectified, left_image_pub_);
+            publishImage(right_rectified, right_image_pub_);
         }
         else
         {
@@ -194,10 +134,18 @@ private:
         }
     }
 
+    void publishImage(const cv::Mat &image, const rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr &pub)
+    {
+        sensor_msgs::msg::Image::SharedPtr img_msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", image).toImageMsg();
+        img_msg->header.stamp = this->now();
+        img_msg->header.frame_id = "camera_frame"; // Set frame ID properly
+        pub->publish(*img_msg);
+    }
+
     // Private members
     sl_oc::video::VideoCapture video_capture_;
     sl_oc::sensors::SensorCapture sensor_capture_;
-
+    cv::Mat K_l, K_r, P_l, P_r, R_l, R_r, D_l, D_r;
     cv::Mat M1l, M2l, M1r, M2r;
 
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr left_image_pub_;
