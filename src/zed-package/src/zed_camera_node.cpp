3bcdc4ec80ebeb62db30b56e6b2b6f75f8839d84
Alexander Fritsch <alexander.fritsch@gt-arc.com>
2024-09-14 20:05:10 +0200

    add info msgs

--- src/zed-package/src/zed_camera_node.cpp
+++ src/zed-package/src/zed_camera_node.cpp
@@ -14,10 +14,13 @@ public:
     {
         // Initialize camera calibration parameters
         initializeCalibrationParams();
+        initializeCameraInfoMsgs();
 
         // Initialize publishers
-        left_image_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/camera/left/image_rect", 10);
-        right_image_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/camera/right/image_rect", 10);
+        left_image_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/left/image_rect", 10);
+        right_image_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/right/image_rect", 10);
+        left_info_pub_ = this->create_publisher<sensor_msgs::msg::CameraInfo>("/left/camera_info", 10);
+        right_info_pub_ = this->create_publisher<sensor_msgs::msg::CameraInfo>("/right/camera_info", 10);
         imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>("/imu", 10);
 
         // Initialize the camera
@@ -76,10 +79,49 @@ private:
         cv::initUndistortRectifyMap(K_r, D_r, R_r, P_r(cv::Range(0, 3), cv::Range(0, 3)), cv::Size(IMAGE_WIDTH, IMAGE_HEIGHT), CV_32F, M1r, M2r);
     }
 
+    void initializeCameraInfoMsgs()
+    {
+        // Left Camera Info Message
+        left_info_msg_.header.frame_id = "camera_left_frame";
+        left_info_msg_.width = IMAGE_WIDTH;
+        left_info_msg_.height = IMAGE_HEIGHT;
+
+        left_info_msg_.k = {K_l.at<double>(0, 0), K_l.at<double>(0, 1), K_l.at<double>(0, 2),
+                            K_l.at<double>(1, 0), K_l.at<double>(1, 1), K_l.at<double>(1, 2),
+                            K_l.at<double>(2, 0), K_l.at<double>(2, 1), K_l.at<double>(2, 2)};
+
+        left_info_msg_.d = {D_l.at<double>(0, 0), D_l.at<double>(0, 1), D_l.at<double>(0, 2), D_l.at<double>(0, 3), D_l.at<double>(0, 4)};
+        left_info_msg_.distortion_model = "plumb_bob";
+        left_info_msg_.r = {R_l.at<double>(0, 0), R_l.at<double>(0, 1), R_l.at<double>(0, 2),
+                            R_l.at<double>(1, 0), R_l.at<double>(1, 1), R_l.at<double>(1, 2),
+                            R_l.at<double>(2, 0), R_l.at<double>(2, 1), R_l.at<double>(2, 2)};
+        left_info_msg_.p = {P_l.at<double>(0, 0), P_l.at<double>(0, 1), P_l.at<double>(0, 2), P_l.at<double>(0, 3),
+                            P_l.at<double>(1, 0), P_l.at<double>(1, 1), P_l.at<double>(1, 2), P_l.at<double>(1, 3),
+                            P_l.at<double>(2, 0), P_l.at<double>(2, 1), P_l.at<double>(2, 2), P_l.at<double>(2, 3)};
+
+        // Right Camera Info Message
+        right_info_msg_.header.frame_id = "camera_right_frame";
+        right_info_msg_.width = IMAGE_WIDTH;
+        right_info_msg_.height = IMAGE_HEIGHT;
+
+        right_info_msg_.k = {K_r.at<double>(0, 0), K_r.at<double>(0, 1), K_r.at<double>(0, 2),
+                             K_r.at<double>(1, 0), K_r.at<double>(1, 1), K_r.at<double>(1, 2),
+                             K_r.at<double>(2, 0), K_r.at<double>(2, 1), K_r.at<double>(2, 2)};
+        right_info_msg_.d = {D_r.at<double>(0, 0), D_r.at<double>(0, 1), D_r.at<double>(0, 2), D_r.at<double>(0, 3), D_r.at<double>(0, 4)};
+        right_info_msg_.distortion_model = "plumb_bob";
+        right_info_msg_.r = {R_r.at<double>(0, 0), R_r.at<double>(0, 1), R_r.at<double>(0, 2),
+                             R_r.at<double>(1, 0), R_r.at<double>(1, 1), R_r.at<double>(1, 2),
+                             R_r.at<double>(2, 0), R_r.at<double>(2, 1), R_r.at<double>(2, 2)};
+        right_info_msg_.p = {P_r.at<double>(0, 0), P_r.at<double>(0, 1), P_r.at<double>(0, 2), P_r.at<double>(0, 3),
+                             P_r.at<double>(1, 0), P_r.at<double>(1, 1), P_r.at<double>(1, 2), P_r.at<double>(1, 3),
+                             P_r.at<double>(2, 0), P_r.at<double>(2, 1), P_r.at<double>(2, 2), P_r.at<double>(2, 3)};
+    }
+
     void captureAndPublish()
     {
         captureAndPublishIMU();
         captureAndPublishImages();
+        publishCameraInfo();
     }
 
     void captureAndPublishIMU()
@@ -98,7 +140,7 @@ private:
         imu_pub_->publish(imu_msg);
 
         RCLCPP_INFO(this->get_logger(), "IMU data: aX=%f, aY=%f, aZ=%f, gX=%f, gY=%f, gZ=%f",
-                     imu_data.aX, imu_data.aY, imu_data.aZ, imu_data.gX, imu_data.gY, imu_data.gZ);
+                    imu_data.aX, imu_data.aY, imu_data.aZ, imu_data.gX, imu_data.gY, imu_data.gZ);
     }
 
     void captureAndPublishImages()
@@ -143,14 +185,26 @@ private:
         pub->publish(*img_msg);
     }
 
+    void publishCameraInfo()
+    {
+        RCLCPP_INFO(this->get_logger(), "Publishing camera info");
+        left_info_pub_->publish(left_info_msg_);
+        right_info_pub_->publish(right_info_msg_);
+    }
+
     // Private members
     sl_oc::video::VideoCapture video_capture_;
     sl_oc::sensors::SensorCapture sensor_capture_;
     cv::Mat K_l, K_r, P_l, P_r, R_l, R_r, D_l, D_r;
     cv::Mat M1l, M2l, M1r, M2r;
 
+    sensor_msgs::msg::CameraInfo left_info_msg_;
+    sensor_msgs::msg::CameraInfo right_info_msg_;
+
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr left_image_pub_;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr right_image_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr left_info_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr right_info_pub_;
     rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
 
     rclcpp::TimerBase::SharedPtr timer_;
