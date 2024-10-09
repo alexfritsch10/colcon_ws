521964c50af5f3ca09c058755f91be9b8333fe94
Alexander Fritsch <alexfritsch10@gmail.com>
2024-10-09 23:06:26 +0200

    tune SGBM params

--- src/zed-package/src/zed_rgbd_node.cpp
+++ src/zed-package/src/zed_rgbd_node.cpp
@@ -134,10 +134,10 @@ private:
             left_matcher->setP1(8 * 3 * 9);
             left_matcher->setP2(32 * 3 * 9);
             left_matcher->setDisp12MaxDiff(96);
-            left_matcher->setMode(cv::StereoSGBM::MODE_SGBM);
-            left_matcher->setPreFilterCap(63);
+            left_matcher->setMode(cv::StereoSGBM::MODE_SGBM_3WAY);
+            left_matcher->setPreFilterCap(31);
             left_matcher->setUniquenessRatio(5);
-            left_matcher->setSpeckleWindowSize(255);
+            left_matcher->setSpeckleWindowSize(31);
             left_matcher->setSpeckleRange(1);
             end = std::chrono::high_resolution_clock::now();
             duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
